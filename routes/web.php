cf669910be2fa1a1c48970bf760f08c2824d1e84
David Carr <dave@dcblog.dev>
2025-03-09 00:28:09 +0000

    upgraded to laravel 12

--- routes/web.php
+++ routes/web.php
@@ -8,9 +8,8 @@ Route::get('/', function () {
 });
 
 Route::get('docs', function () {
-    return redirect('docs/v11/introduction');
+    return redirect('docs/v12/introduction');
 });
 
 Route::get('docs/{path}', [PageController::class, 'page'])->name('docs.page')->where(['path' => '.*']);
 Route::view('become-a-sponsor', 'become-a-sponsor')->name('become-a-sponsor');
-
