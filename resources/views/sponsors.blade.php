d246afe990e7893a261e44f25d70bd6bcfc9c1dd
David Carr <dave@dcblog.dev>
2025-05-22 16:41:16 +0100

    added NativePHP sponsor links and images to sponsors and toc-and-sponsors views

--- resources/views/sponsors.blade.php
+++ resources/views/sponsors.blade.php
@@ -1,12 +1,12 @@
 <div class="container my-20 mx-auto">
     <div class="grid gap-5 max-w-lg mx-auto lg:grid-cols-4 lg:max-w-none">
 
-        <a href="https://dcblog.dev?utm_source=laravelmodules.com&utm_medium=website" target="_blank" class="relative w-full rounded-lg border-2 border-dashed border-gray-300 px-12 py-8 hover:border-sky-300 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 lg:col-span-1 flex gap-2 items-center">
-            <img src="{{ url('images/sponsors/dcblog.png') }}" alt="David Carr's Blog" title="David Carr's Blog">
+        <a href="https://nativephp.com/mobile" class="relative w-full rounded-lg border-2 border-dashed border-gray-300 px-12 py-8 hover:border-sky-300 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 lg:col-span-1 flex gap-2 items-center">
+            <img src="{{ url('images/sponsors/NativePHP-mobile.svg') }}" alt="NativePHP" title="NativePHP">
         </a>
 
-        <a href="/become-a-sponsor" class="relative w-full rounded-lg border-2 border-dashed border-gray-300 px-12 py-8 hover:border-sky-300 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 lg:col-span-1 flex gap-2 items-center">
-            <span class="text-base font-semibold text-gray-400">Your&nbsp;Logo</span>
+        <a href="https://dcblog.dev?utm_source=laravelmodules.com&utm_medium=website" target="_blank" class="relative w-full rounded-lg border-2 border-dashed border-gray-300 px-12 py-8 hover:border-sky-300 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 lg:col-span-1 flex gap-2 items-center">
+            <img src="{{ url('images/sponsors/dcblog.png') }}" alt="David Carr's Blog" title="David Carr's Blog">
         </a>
 
         <a href="/become-a-sponsor" class="relative w-full rounded-lg border-2 border-dashed border-gray-300 px-12 py-8 hover:border-sky-300 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 lg:col-span-1 flex gap-2 items-center">
